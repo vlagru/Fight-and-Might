a1a3482d0992c5e9522e86de595cfcbfb758062d
vlagru <v.grulich@seznam.cz>
2022-03-19 12:22:20 +0100

    Add Kim Jong Il

--- src/org/vlagru/Bojovnik.java
+++ src/org/vlagru/Bojovnik.java
@@ -13,6 +13,8 @@ public class Bojovnik {
     private int obrana;
     /** Instance hraci kostky */
     private Kostka kostka;
+    /** Vraci zpravy o utoku a obrane */
+    private String zprava;
 
     public Bojovnik(String jmeno, int zivot,
                     int utok, int obrana, Kostka kostka) {
@@ -30,7 +32,7 @@ public class Bojovnik {
     public String grafickyZivot() {
         String s = "[";
         int celkem = 20;
-        //TODO zivot & zivotMax
+        //TODO zivot & zivotMax, zjistit detaily
         double pocet = Math.round(((double)zivot/maxZivot) * celkem);
         if((pocet == 0) && (nazivu())) {
             pocet = 1;
@@ -43,6 +45,48 @@ public class Bojovnik {
         }
         s += "]";
         return s;
+    }
+
+    /**
+     * Obrana soupere pred utokem bojovnika (viz reference).
+     * @param uder
+     */
+    public void branSe(int uder) {
+        int zraneni = uder - (obrana + kostka.hod());
+        if (zraneni > 0) {
+            zivot -= zraneni;
+            zprava = String.format("%s utrpel poskozeni %s hp", jmeno, zraneni);
+            if (zivot <= 0) {
+                zivot = 0;
+                zprava += " a zemrel";
+            }
+        }
+        else zprava = String.format("%s odrazil utok", jmeno);
+        nastavZpravu(zprava); // TODO proc je zprava ulozena v else a nikoliv po ukonceni cele podminky?
+    }
+
+    /**
+     * Obe reference 'bojovnik' a 'souper' ukazuji na spolecnou referenci 'bojovnik'.
+     * Utok bojovnika na soupere, tedy 'Volodi' na 'Volodu'.
+     * @param
+     */
+    public void utoc(Bojovnik bojovnik) {
+        int uder = utok + kostka.hod();
+        nastavZpravu(String.format("%s utoci uderem za %s hp", jmeno, uder));
+        bojovnik.branSe(uder);
+    }
+
+    //TODO : Zjistit, proc 'konstruktor' tohoto typu je 'private' a nenese jmeno tridy.
+    private void nastavZpravu(String zprava) {
+        this.zprava = zprava;
+    }
+
+    /**
+     * Vraci zpravu o utoku a obrane bojovnika a soupere.
+     * @return Vraci zpravu o utoku a obrane bojovnika a soupere.
+     */
+    public String vratPosledniZpravu() {
+        return zprava;
     }
 
     @Override
--- src/org/vlagru/Program.java
+++ src/org/vlagru/Program.java
@@ -7,10 +7,19 @@ public class Program {
         Kostka kostka = new Kostka(10);
         Bojovnik bojovnik = new Bojovnik("Voloda",100, 20, 10, kostka);
 
-        System.out.printf("Bojovnik: %s\n", bojovnik);
-        System.out.printf("Nazivu: %s\n", bojovnik.nazivu());
+//        System.out.printf("Bojovnik: %s\n", bojovnik);
+//        System.out.printf("Nazivu: %s\n", bojovnik.nazivu());
         System.out.printf("Zivot: %s\n", bojovnik.grafickyZivot());
 
+        Bojovnik souper = new Bojovnik("Kim Jong Il", 80, 25, 7, kostka);
+        souper.utoc(bojovnik);
+        System.out.println(souper.vratPosledniZpravu());
+        System.out.println(bojovnik.vratPosledniZpravu());
+        System.out.printf("Zivot: %s\n", bojovnik.grafickyZivot());
+
+/**
+ * Hazeni sestistennou a osmistennou kostkou
+ */
 //        Kostka sestistenna = new Kostka();
 //        Kostka desetistenna = new Kostka(10);
 //
